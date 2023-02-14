13fdedd8f12fd521494d79127aec3ffd664b52be
J.hoon <68211978+Choi-jeonghoon@users.noreply.github.com>
2023-02-14 15:33:18 +0900

    Add:Card UI로 map 변경

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import img2 from "./img/img1.jpg";
-import { Navbar, Container, Nav, Row, Col } from "react-bootstrap/";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Row,
+  Col,
+  Card,
+  Button,
+} from "react-bootstrap/";
 import { data } from "./data/dataInfo";
 import { useState } from "react";
 
@@ -27,14 +35,27 @@ function App() {
       <Container>
         <Row>
           {shoes.map((data) => (
-            <Col key={data.id}>
-              <img
+            // <Col key={data.id}>
+            //   <img
+            //     src="https://codingapple1.github.io/shop/shoes2.jpg"
+            //     style={{ width: "100%" }}
+            //   />
+            //   <h4>{data.title}</h4>
+            //   <p>{data.content}</p>
+            //   <p>{data.price}</p>
+            // </Col>
+            <Card key={data.id} style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
                 src="https://codingapple1.github.io/shop/shoes2.jpg"
-                style={{ width: "100%" }}
               />
-              <h4>{data.title}</h4>
-              <p>{data.price}</p>
-            </Col>
+              <Card.Body>
+                <Card.Title>{data.title}</Card.Title>
+                <Card.Text>{data.content}</Card.Text>
+                <Card.Text>{data.price}</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
           ))}
         </Row>
       </Container>
