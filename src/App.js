8104ab508d21d9edac3bb97b448db38ef2e45e64
J.hoon <68211978+Choi-jeonghoon@users.noreply.github.com>
2023-02-14 14:43:42 +0900

    Add:state 관리

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import img2 from "./img/img1.jpg";
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap/";
+import { data } from "./data/dataInfo";
+import { useState } from "react";
 
 function App() {
+  const [shoes] = useState(data);
+  console.log(shoes[0]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Brand</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div
+        className="main-bg"
+        // style={{ backgroundImage: "url(" + img2 + ")" }}
+      ></div>
+      <Container>
+        <Row>
+          <Col>
+            <img
+              src={process.env.PUBLIC_URL + "/logo192.png"}
+              style={{ width: "100px" }}
+            />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
+          </Col>
+          <Col>
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              style={{ width: "100px" }}
+            />
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
+          </Col>
+          <Col>
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              style={{ width: "100px" }}
+            />
+            <h4>{shoes[3].title}</h4>
+            <p>{shoes[3].price}</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/data/dataInfo.js
@@ -0,0 +1,33 @@
+const data = [
+  {
+    id: 0,
+    title: "Black",
+    content: "F",
+    price: 9659,
+  },
+  {
+    id: 1,
+    title: "Black",
+    content: "F",
+    price: 9459,
+  },
+  {
+    id: 2,
+    title: "Black",
+    content: "F",
+    price: 45675679,
+  },
+  {
+    id: 3,
+    title: "Black",
+    content: "F",
+    price: 535342,
+  },
+  {
+    id: 4,
+    title: "Black",
+    content: "F",
+    price: 123123,
+  },
+];
+export { data };
